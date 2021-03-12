2ee093a22d9f3d892c92589851bdf2b33ca5561a
mrdulin <novaline.dulin@gmail.com>
2021-03-12 13:00:19 +0800

    test: update example

--- issues/expect-react-component/index.test.tsx
+++ issues/expect-react-component/index.test.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { Loading, MyComponent } from './';
 import ReactDOM from 'react-dom';
 
@@ -12,8 +13,8 @@ describe('MyComponent', () => {
     // 2. TS type error
     // expect(renderSpy).toBeCalledWith(Loading, loadingContainer);
 
-    // 3. TS type error
-    // expect(renderSpy).toBeCalledWith(<Loading />, loadingContainer);
+    // 3. TS type error, but works if type case to any
+    expect(renderSpy).toBeCalledWith((<Loading className="loading" />) as any, loadingContainer);
 
     // 4. success!
     expect(renderSpy).toBeCalledWith(renderSpy.mock.calls[0][0], loadingContainer);
