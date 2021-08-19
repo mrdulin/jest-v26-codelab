011ef4546e40a23f4d23b0cff0d18c665660bbda
mrdulin <novaline.dulin@gmail.com>
2021-08-19 13:08:15 +0800

    test: add jest transform example

---
+++ examples/68838761/index.test.ts
@@ -0,0 +1,8 @@
+//@ts-ignore
+import image1Src from './assets/img1.png';
+
+describe('68838761', () => {
+  test('should pass', () => {
+    console.log(image1Src);
+  });
+});
--- jest.config.js
+++ jest.config.js
@@ -10,4 +10,8 @@ module.exports = {
   testEnvironmentOptions: {
     enzymeAdapter: 'react16',
   },
+  // transform: {
+  //   '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+  //     '<rootDir>/examples/68838761/fileTransformer.js',
+  // },
 };
