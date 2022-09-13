8cdd7bd3ab66dc819a210b46b32c898dec378ed6
official_dulin <novaline.dulin@gmail.com>
2022-09-13 03:36:23 +0000

    test: redux reducer function

---
+++ stackoverflow/73694887/index.test.ts
@@ -0,0 +1,52 @@
+const initialPeopleState = {};
+
+const DELETE_PERSON = 'DELETE_PERSON';
+const PeopleReducer = (state: any = initialPeopleState, action: any): object => {
+  switch (action.type) {
+    case DELETE_PERSON:
+      return { ...state, people: state.people?.filter((a: any) => a.id !== action.id) };
+    default:
+      return state;
+  }
+};
+
+export const deletePerson = (id: number) => {
+  return {
+    type: DELETE_PERSON,
+    id,
+  };
+};
+
+describe('73694887', () => {
+  test('should pass', () => {
+    const mockData = {
+      people: [
+        {
+          id: 1,
+          name: 'Leanne Graham',
+          username: 'Bret',
+          email: 'Sincere@april.biz',
+          address: {
+            street: 'Kulas Light',
+            suite: 'Apt. 556',
+            city: 'Gwenborough',
+            zipcode: '92998-3874',
+            geo: {
+              lat: '-37.3159',
+              lng: '81.1496',
+            },
+          },
+          phone: '1-770-736-8031 x56442',
+          website: 'hildegard.org',
+          company: {
+            name: 'Romaguera-Crona',
+            catchPhrase: 'Multi-layered client-server neural-net',
+            bs: 'harness real-time e-markets',
+          },
+        },
+      ],
+    };
+
+    expect(PeopleReducer(mockData, deletePerson(1))).toEqual({ people: [] });
+  });
+});
