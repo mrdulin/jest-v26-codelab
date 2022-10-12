8d8cc52a533952d7f3b5d53ea0a87dc264021f08
official_dulin <novaline.dulin@gmail.com>
2022-10-12 02:50:34 +0000

    test: update exam,ple

--- stackoverflow/74025882/foo.sh
+++ stackoverflow/74025882/foo.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
+sleep 6
 echo "fooooo"
--- stackoverflow/74025882/index.test.js
+++ stackoverflow/74025882/index.test.js
@@ -1,14 +1,16 @@
 const cp = require('child_process');
 const path = require('path');
 
-test('script', (done) => {
-  const script = cp.exec(path.resolve(__dirname, './foo.sh'));
-  script.stdout.on('data', (data) => {
-    console.log('stdout: ' + data);
-    done();
-  });
-  script.stderr.on('data', (data) => {
-    console.error('stderr: ' + data);
-    done();
-  });
+describe('74025882', () => {
+  test('script', (done) => {
+    const script = cp.exec(path.resolve(__dirname, './foo.sh'));
+    script.stdout.on('data', (data) => {
+      console.log('stdout: ' + data);
+      done();
+    });
+    script.stderr.on('data', (data) => {
+      console.error('stderr: ' + data);
+      done();
+    });
+  }, 10 * 1_000);
 });
