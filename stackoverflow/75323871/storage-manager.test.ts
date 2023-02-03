324e37cf8a16b359cc50e84199371ab15b76fb44
mrdulin <novaline.dulin@gmail.com>
2023-02-03 14:18:47 +0800

    test: update SO example

--- stackoverflow/75323871/storage-manager.test.ts
+++ stackoverflow/75323871/storage-manager.test.ts
@@ -2,10 +2,23 @@ import { CookiesManager, a, b } from './storage-manager';
 
 jest.mock('./storage-manager', () => {
   const originalModules = jest.requireActual('./storage-manager');
+
+  const mockCookies = {};
   class CookiesManagerMock {
-    static get = jest.fn();
-    static set = jest.fn();
-    static remove = jest.fn();
+    static set = jest.fn().mockImplementation((key, value, config) => {
+      console.log('Mock set called');
+      mockCookies[key] = value;
+    });
+
+    static get = jest.fn().mockImplementation((key) => {
+      console.log('Mock get called');
+      return mockCookies[key];
+    });
+
+    static remove = jest.fn().mockImplementation((key, config) => {
+      console.log('Mock remove called');
+      delete mockCookies[key];
+    });
   }
   return {
     ...originalModules,
@@ -19,6 +32,9 @@ describe('75323871', () => {
     expect(jest.isMockFunction(CookiesManager.set)).toBeTruthy();
     CookiesManager.set('test_key', 'test_value', config);
     expect(CookiesManager.set).toHaveBeenCalledWith('test_key', 'test_value', config);
+    expect(CookiesManager.get('test_key')).toBe('test_value');
+
+    // other named exports
     expect(a()).toBe('a');
     expect(b()).toBe('b');
   });
