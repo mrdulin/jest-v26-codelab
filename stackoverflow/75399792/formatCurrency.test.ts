604a445c88b6685a9ec52fb410e5e2f4d7e98c58
mrdulin <novaline.dulin@gmail.com>
2023-02-10 19:18:49 +0800

    test: update SO example

--- stackoverflow/75399792/formatCurrency.test.ts
+++ stackoverflow/75399792/formatCurrency.test.ts
@@ -5,6 +5,7 @@ describe('given formatCurrency fucntion', () => {
 
   beforeEach(() => {
     windowSpy = jest.spyOn(window, 'window', 'get');
+    jest.resetModuleRegistry();
   });
 
   afterEach(() => {
@@ -22,5 +23,18 @@ describe('given formatCurrency fucntion', () => {
 
     expect(window.navigator.language).toBe('en-GB');
     expect(formatCurrency(1000)).toBe('£1,000.00');
+  });
+
+  it('should set the user language to `en-US`', async () => {
+    windowSpy.mockImplementation(() => ({
+      navigator: {
+        language: 'en-US',
+      },
+    }));
+
+    const { formatCurrency } = await import('./formatCurrency');
+
+    expect(window.navigator.language).toBe('en-US');
+    expect(formatCurrency(1000)).toBe('$1,000.00');
   });
 });
