18d91736a9aaad0ab996a812e72ca52c147005d9
mrdulin <novaline.dulin@gmail.com>
2023-04-27 15:02:40 +0800

    test: improve SO example

--- stackoverflow/76117012/App.test.tsx
+++ stackoverflow/76117012/App.test.tsx
@@ -5,8 +5,9 @@ import App from './App';
 
 test('testing onchange event for oshop', (done) => {
   render(<App />);
-  const dropdElement = screen.queryByTestId("Oshop")
-  if (!dropdElement) return done('select element not found')
+  const dropdElement = screen.queryByTestId("Oshop");
+  expect(dropdElement).toHaveValue(undefined);
+  if (!dropdElement) return done('select element not found');
   fireEvent.change(dropdElement, { target: { value: "meat" } });
   expect(screen.getByTestId('Oshop')).toHaveValue('meat');
   done();
