53f5d51dd5d07f719829d42cde8f1612ffc46e1f
mrdulin <novaline.dulin@gmail.com>
2023-04-27 14:58:08 +0800

    test: add SO example

---
+++ stackoverflow/76117012/App.test.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import App from './App';
+
+test('testing onchange event for oshop', (done) => {
+  render(<App />);
+  const dropdElement = screen.queryByTestId("Oshop")
+  if (!dropdElement) return done('select element not found')
+  fireEvent.change(dropdElement, { target: { value: "meat" } });
+  expect(screen.getByTestId('Oshop')).toHaveValue('meat');
+  done();
+});
---
+++ stackoverflow/76117012/App.tsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+function App() {
+  const [selectedValue, setSelectedValue] = useState()
+  function selectionChange(value) {
+    console.log(value)
+    setSelectedValue(value);
+  }
+  return (
+    <div className="App">
+      <select value={selectedValue} data-testid="Oshop" onChange={(e) => selectionChange(e.target.value)}>
+        <option value="fruit">Fruit</option>
+        <option value="vegetable">Vegetable</option>
+        <option value="meat">Meat</option>
+      </select>
+    </div>
+  );
+}
+
+export default App;
