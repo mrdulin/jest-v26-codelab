845bf45726c9d46a3e20a42caa28dec35324a6f7
mrdulin <novaline.dulin@gmail.com>
2023-04-27 21:32:53 +0800

    test: add rerender API example

---
+++ stackoverflow/76118663/index.test.tsx
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+
+const Button = (props: React.ButtonHTMLAttributes<HTMLButtonElement>) => <button {...props}>click me</button>
+
+describe('76118663', () => {
+  let rerender: ReturnType<typeof render>['rerender'];
+  const props: React.ButtonHTMLAttributes<HTMLButtonElement> = { type: 'button' };
+  beforeEach(() => {
+    rerender = render(<Button {...props} />).rerender;
+  });
+  test('should override button disabled prop', () => {
+    expect(screen.queryByRole('button')).toBeEnabled();
+    rerender(<Button {...props} disabled />);
+    expect(screen.queryByRole('button')).toBeDisabled();
+  });
+
+  test('should override button type prop and reset disabled prop', () => {
+    rerender(<Button {...props} type='submit' />);
+    expect(screen.queryByRole('button')).toBeEnabled();
+    expect(screen.queryByRole('button')).toHaveAttribute('type', 'submit');
+  })
+})
