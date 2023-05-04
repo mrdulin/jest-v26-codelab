def1251d614fb6a82fad2a932e9ce4b9ab69c410
mrdulin <novaline.dulin@gmail.com>
2023-05-04 14:58:58 +0800

    test: add ts-mockito example

---
+++ stackoverflow/76133872/index.test.ts
@@ -0,0 +1,30 @@
+import { mock, instance, anything, when } from 'ts-mockito'
+
+class B {
+  public doTheThing(str: string): number {
+    return str.length;
+  }
+}
+
+class A {
+  private readonly b: B;
+  constructor(b: B) {
+    this.b = b;
+  }
+
+  public doTheMainThing() {
+    return this.b.doTheThing('strrr');
+  }
+}
+
+describe('test cases', () => {
+  it('controls the mocked method', () => {
+    const mockedB: B = mock(B);
+    const instanceB: B = instance(mockedB);
+    when(mockedB.doTheThing(anything())).thenReturn(4);
+
+    const a = new A(instanceB);
+    const actual = a.doTheMainThing();
+    expect(actual).toBe(4);
+  });
+});
