83a34385d07f383e3abed8dd13f307457153eb08
mrdulin <novaline.dulin@gmail.com>
2023-05-17 17:35:59 +0800

    feat: add toHaveProperty example

---
+++ stackoverflow/76269234/index.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from "@testing-library/react"
+
+describe('76269234', () => {
+  test('should pass', () => {
+    render(<div>
+      <input type='text' />
+      <input maxLength={1} type='text' />
+    </div>)
+    const textbox = screen.getAllByRole('textbox') as HTMLInputElement[]
+    console.log(textbox[1].maxLength)
+    expect(textbox[1]).toHaveProperty('maxLength', 1)
+  })
+})
